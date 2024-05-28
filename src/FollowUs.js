cb43e5f820a8507b27b5f7f2d0197efcc6db15cf
servant-savio <ericcurry447@gmail.com>
2024-05-28 03:57:35 +0000

    Change order of follow

--- src/FollowUs.js
+++ src/FollowUs.js
@@ -4,12 +4,12 @@ import mysticX from './images/ProjectMysticDVX.png';
 const FollowUs = () => {
   return (
     <div>
-      Follow us:
-      <div>
-        <a href="https://x.com/ServantSavio" target='_blank' rel="noreferrer"><img src={savioX} /></a>
-      </div>
+      <h3>Follow us</h3>
       <div>
         <a href="https://x.com/ProjectMysticDV" target='_blank' rel="noreferrer"><img src={mysticX} /></a>
+      </div>
+      <div>
+        <a href="https://x.com/ServantSavio" target='_blank' rel="noreferrer"><img src={savioX} /></a>
       </div>
     </div>
   );
