08d46ac7346273bbd311508531c6e0b17d99445c
Savio <ericcurry447@gmail.com>
2024-05-30 04:57:29 -0700

    Prayers

--- src/MainBody.js
+++ src/MainBody.js
@@ -4,16 +4,20 @@ import Home from './Home';
 import Rosary from './rosary/Rosary';
 import DivineMercy from './DivineMercy';
 import About from './About';
-  
+import YouTube from './YouTube';
+import Prayers from './Prayers';
+
 const MainBody = () => {
   return (
     <main>
       <Routes>
         <Route path="/mystic/" element={<Home />} />
+        <Route path="/mystic/youtube" element={<YouTube />} />
         <Route path="/mystic/home" element={<Home />} />
         <Route path="/mystic/rosary/*" element={<Rosary />} />
         <Route path="/mystic/divine-mercy" element={<DivineMercy />} />
         <Route path="/mystic/about" element={<About />} />
+        <Route path="/mystic/prayers" element={<Prayers />} />
       </Routes>
     </main>
   );
---
+++ src/Prayers.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import prayersHeader from './images/prayers-header.png';
+
+const Prayers = () => {
+  return (
+    <div>
+      <img src={prayersHeader} alt="Prayers"/>
+      <div>
+
+      </div>
+      <div className='outerPrayer'>
+        <div className='prayerContainer'>
+        <h2>MORNING OFFERING</h2>
+        <p>
+        O Jesus, through the Immaculate Heart of Mary, 
+        I offer you my prayers, works, joys and sufferings 
+        of this day for all the intentions of your Sacred 
+        Heart, in union with the Holy Sacrifice of the Mass 
+        throughout the world, in reparation for my sins, 
+        for the intentions of all my relatives and friends 
+        and in particular for the intentions of the Holy 
+        Father. Amen.
+        </p>
+        <h2>ACT OF FAITH</h2>
+        <p>
+          O my God, I firmly believe that you are one 
+          God in three divine persons, Father, Son and 
+          Holy Spirit; I believe that your divine Son 
+          became man and died for our sins, and that 
+          he shall come to judge the living and the 
+          dead. I believe these and all the truths that 
+          the holy Catholic Church teaches, because you 
+          have revealed them, who can neither deceive nor 
+          be deceived.
+        </p>
+        <h2>ACT OF HOPE</h2>
+        <p>
+          O my God, relying on your almighty power and 
+          infinite mercy and promises, I hope to obtain 
+          pardon for my sins, the help of your grace, 
+          and life everlasting, through the merits of 
+          Jesus Christ, my Lord and Redeemer.
+        </p>
+
+        <h2>ACT OF CHARITY</h2>
+        <p>
+        O my God, I love you above all things, with my 
+        whole heart and soul, because you are all-good and 
+        worthy of all love. I love my neighbor as myself for 
+        the love of you. I forgive all who have injured me, 
+        and ask pardon of all whom I have injured.
+        </p>
+        <h2>PRAYER BEFORE A DAY'S WORK</h2>
+        <p>
+          Direct, we beg you, O Lord, our actions by your holy 
+          inspirations, and carry them on by your gracious 
+          assistance, that every prayer and work of ours may begin 
+          always with you, and through you be happily, ended. 
+          Amen.
+        </p>
+        <h2>
+          PRAYER TO KEEP PRESENCE OF GOD
+        </h2>
+        <p>
+          Lord, God Almighty, you have brought us safely to the 
+          beginning of this day. Defend us today by your mighty 
+          power, that we may not fall into any sin, but that all 
+          our words may so proceed and all our thoughts and actions
+          be so directed, as to be always just in your sight. 
+          Through Christ our Lord. Amen.
+        </p>
+
+        <h2>
+          ACCEPTANCE OF GOD'S WILL
+        </h2>
+        <p>
+          In all things may the most holy, the most just, and 
+          the most lovable will of God be done, praised, and exalted 
+          above all for ever. Your will be done, O Lord, your will 
+          be done. The Lord has given, the Lord has taken away; 
+          blessed be the name of the Lord.
+        </p>
+        <h2>
+          PRAYER FOR VOCATIONS
+        </h2>
+        <p>
+          Lord Jesus Christ, Shepherd of souls, who called the apostles 
+          to be fishers of men, raise up new apostles in your holy 
+          Church. Teach them that to serve you is to reign: to 
+          possess you is to possess all things. Kindle in the young 
+          hearts of our people the fire of zeal for souls. Make them 
+          eager to spread your Kingdom upon earth. Grant them courage 
+          to follow you, who are the Way, the Truth and the Life; who 
+          lives and reigns for ever and ever. Amen.
+        </p>
+        <h2>
+          ACT OF CONTRITION
+        </h2>
+        <p>
+          O my God, I am heartily sorry for having offended you and 
+          I detest all my sins, because I dread the loss of heaven 
+          and the pains of hell; but most of all because they offend 
+          you, my God, who are all good and deserving of all my love. 
+          I firmly resolve, with the help of your grace, to confess 
+          my sins, to do penance, and to amend my life. Amen.
+        </p>
+        <h2>
+          PRAYER FOR THE POPE
+        </h2>
+        <p>
+          May the Lord preserve him and give him life, and make him
+          blessed upon the earth, and deliver him not to the will of his
+          enemies. Amen.
+        </p>
+        <h2>
+          COME HOLY SPIRIT
+        </h2>
+        <p>
+          Come Holy Spirit, fill the hearts of your faithful and kindle in them the fire of your love. Send forth your Spirit and they shall be created. And You shall renew the face of the earth.
+          O, God, who by the light of the Holy Spirit, did instruct the hearts of the faithful, grant that by the same Holy Spirit we may be truly wise and ever enjoy His consolations, Through Christ Our Lord, Amen.
+        </p>
+        <h2>
+          HAIL MARY
+        </h2>
+        <p>
+          Hail Mary full of Grace, the Lord is with thee.
+          Blessed are thou amongst women and blessed is the fruit of thy womb Jesus. 
+        </p>
+        <p>
+          Holy Mary Mother of God,
+          pray for us sinners now and at the hour of our death
+        </p>
+        <p>
+          Amen.
+        </p>
+        <h2>
+          HAIL HOLY QUEEN
+        </h2>
+        <p>
+          Hail, holy Queen, Mother of mercy, hail, our life, our 
+          sweetness and our hope. To thee do we cry, poor banished 
+          children of Eve: to thee do we send up our sighs, 
+          mourning and weeping in this vale of tears. Turn then, 
+          most gracious Advocate, thine eyes of mercy toward us, 
+          and after this our exile, show unto us the blessed 
+          fruit of thy womb, Jesus, O merciful, O loving, O 
+          sweet Virgin Mary! Amen.
+        </p>
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+export default Prayers;
