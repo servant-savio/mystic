9b9746845021dbab41774761fb059db3ea888e78
Savio <ericcurry447@gmail.com>
2024-06-05 11:00:44 -0700

    Fix typos

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ const About = () => {
     <div className='generalOuterContainer'>
     <div className='generalContainer'>
       <h1>About Project Mystic</h1>
-      <p>This website is currently an independent effort by <a href="https://x.com/ServantSavio" target="_blank" rel="noreferrer">Savio</a> drawing inspiration from the X Project Mystic apostolate lead by Arvin using <a href="https://x.com/ProjectMysticDV"  target="_blank" rel="noreferrer">@ProjectMysticDV</a> handle. Arin owns the Project Mystic logo and name</p>
+      <p>This website is currently an independent effort by <a href="https://x.com/ServantSavio" target="_blank" rel="noreferrer">Savio</a> drawing inspiration from the X Project Mystic apostolate lead by Arvin Lee using <a href="https://x.com/ProjectMysticDV"  target="_blank" rel="noreferrer">@ProjectMysticDV</a> handle. Arvin owns the Project Mystic logo and name</p>
       <p>The overall goal of this website is to become a tool that one can carry around that will have common prayers that one can use throughout the day, every day.</p>
       <p>So far, it's a one-man effort mostly done in one weekend. The hope that it will be more useful in the coming days or months.</p>
       <p>The goal is to pray consciously and subconciously, similar to breathing. Being somewhat aware of God's presence every second of every day. All within the spirit of the Project Mystic apostolate.</p>
