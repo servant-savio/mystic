b1fc16c5434483b966bc86f40fd5a6ebda0802af
Savio <ericcurry447@gmail.com>
2024-06-06 02:08:59 -0700

    About title

--- src/About.js
+++ src/About.js
@@ -7,7 +7,7 @@ const About = () => {
   return (
     <div className='generalOuterContainer'>
     <div className='generalContainer'>
-      <h1>About Project Mystic</h1>
+      <h1>About</h1>
       <p>This website is currently an independent effort by <a href="https://x.com/ServantSavio" target="_blank" rel="noreferrer">Savio</a> drawing inspiration from the X Project Mystic apostolate lead by Arvin Lee using <a href="https://x.com/ProjectMysticDV"  target="_blank" rel="noreferrer">@ProjectMysticDV</a> handle. Arvin owns the Project Mystic logo and name</p>
       <p>The overall goal of this website is to become a tool that one can carry around that will have common prayers that one can use throughout the day, every day.</p>
       <p>So far, it's a one-man effort mostly done in one weekend. The hope that it will be more useful in the coming days or months.</p>
