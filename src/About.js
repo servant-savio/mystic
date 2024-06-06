f73399306e33731550ce320420d6428774b17303
Savio <ericcurry447@gmail.com>
2024-06-06 11:47:49 -0700

    About

--- src/About.js
+++ src/About.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import FollowUs from './FollowUs';
+import aboutHeader from './images/aboutHeader.png';
 
 const About = () => {
   const supportLink = "https://myslink.app/projectmysticdv";
   const prayerGuideLink = "https://x.com/ProjectMysticDV/status/1789896228253118721";
   return (
-    <div className='generalOuterContainer'>
+    <div>
+      <img src={aboutHeader} alt="Bible Search"/>
+      <br/>
+      <div className='generalOuterContainer'>
     <div className='generalContainer'>
-      <h1>About</h1>
       <p>This website is currently an independent effort by <a href="https://x.com/ServantSavio" target="_blank" rel="noreferrer">Savio</a> drawing inspiration and guidance from the X Project Mystic apostolate lead by Arvin Lee using <a href="https://x.com/ProjectMysticDV"  target="_blank" rel="noreferrer">@ProjectMysticDV</a> handle. Arvin owns the Project Mystic logo and name</p>
       <p>The overall goal of this website is to become a tool that one can carry around that will have common prayers that one can use throughout the day, every day.</p>
       <p>So far, it's a one-man effort mostly done in one weekend. The hope that it will be more useful in the coming days or months.</p>
@@ -18,6 +21,7 @@ const About = () => {
       <h2>Prayer Guide</h2>
       <a href={prayerGuideLink} target="_blank" rel="noreferrer">X Prayer Guide</a>
       <FollowUs />
+    </div>
     </div>
     </div>
   );
