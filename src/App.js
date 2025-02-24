70bed9f27b24381beae8f100f3edc62c602b604b
Savio <ericcurry447@gmail.com>
2025-02-23 17:44:26 -0800

    404 handler

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, useNavigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
 import Header from './Header';
 import Nav from './Nav';
 import './App.css';
 import MainBody from './MainBody';
 import PreLoad from './PreLoad';
 
+function RedirectHandler() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+      const params = new URLSearchParams(location.search);
+      const refPath = params.get("ref");
+
+      if (refPath) {
+          navigate(refPath, { replace: true });
+      }
+  }, [location, navigate]);
+
+  return null; // No UI needed, just performs redirection
+}
+
 function App() {
   return (
     <Router>
+      <RedirectHandler />
       <div className="App">
         <PreLoad />
         <Header />
